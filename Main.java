7050955f617fb070a4fea3fdfb46404967705a1b
~ Ananya ~ <ananya230106@gmail.com>
2021-05-08 11:21:04 +0000

    README.md

--- Main.java
+++ Main.java
@@ -1,22 +1,31 @@
-//
+//Importing java.io package 
 import java.io.*;
 
-class Main 
+//Name the Class
+class Main
+
 {
-  public static void main(String args[]) throws IOException
+  public static void main(String args[]) throws IOException /* It means that it remove the input output errors */
 
   {
+    // Making InputStreamReader Object
     InputStreamReader read = new InputStreamReader(System.in);
-
+    // Making BufferedReader Object
     BufferedReader br = new BufferedReader(read);
 
+    // Taking the input from the user(Time in seconds)
     System.out.print("Time in seconds: ");
+    // Storing the input value in a variable
     int inputTime = Integer.parseInt(br.readLine());
 
+    // Calculating the Hours
     int hrs = inputTime / 3600;
+    // Calculating the Minutes
     int mins = (inputTime % 3600) / 60;
+    // Calculating the Seconds
     int secs = (inputTime % 3600) % 60;
 
+    // Displaying the Time in Hours , Minutes and Seconds
     System.out.println(hrs + " Hours " + mins + " Minutes " + secs + " Seconds");
   }
 }
---
+++ README.md
@@ -0,0 +1,7 @@
+Write a program to input time in seconds. 
+
+Display the time after converting them into hours, minutes and seconds.
+
+Sample Input: Time in seconds: 5420
+
+Sample Output: 1 Hour 30 Minutes 20 Seconds
